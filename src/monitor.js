042a1fbb3853bacfc7468647e13b149b0de6b644
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-03-13 22:32:54 +0100

    OS uptime monitor

--- src/monitor.js
+++ src/monitor.js
@@ -1,13 +1,17 @@
 const os = require('node:os')
 const colors = require("fluent-interface-colors")
 
-function systemMonitor(interval = 1000) {
+function systemMonitor() {
+  /*
+
+    !-- UTILITIES --!
+  
+  */
   const colorizePercentage = (percentage) => {
     if(percentage < 25) return colors.bggreen.format(percentage+"%")
     if(percentage < 75) return colors.bgyellow.format(percentage+"%")
     return colors.bgred.format(percentage+"%")
   }
-
   /*
     Calculate the difference between miliseconds spent in various cpu modes between initialTimes and times,
     then calculate percentage of idle vs non-idle distribution in those deltas
@@ -36,36 +40,30 @@ function systemMonitor(interval = 1000) {
     return [freeRam, colorizePercentage(percentageUsed)]
   }
 
-  const cpus = (interval) => {
-    let initialTimes = os.cpus().map((cpu) => cpu.times)
-
-    return setInterval(() => {
-      const currentTimes = os.cpus().map((cpu) => cpu.times)
-
-      //Clear terminal window
-      process.stdout.write('\x1b[2J\x1b[1;1H')
-
-      let count = 1
-      initialTimes.map((cpu, index) => {
-        const usage = caluclateCPUUsage(initialTimes[index], currentTimes[index])
-        colors.bold.log(`CPU${count}: `, usage)
-        count+=1
-      })
-
-      initialTimes = currentTimes
-    }, interval)
+  const osType = () => {
+    const osName = os.type()
+    switch (osName) {
+      case 'Windows_NT':
+        return colors.bold.blue.format("Windows")
+      case 'Darwin':
+        return colors.bold.cyan.format("MacOS")
+      default:
+        return colors.bold.green.format(osName)
+    }
   }
 
-  const memory = (interval) => {
-    const totalRam = Math.round(100*(os.totalmem()/1073741824))/100
-
-    return setInterval(() => {
-      //Clear terminal window
-      process.stdout.write('\x1b[2J\x1b[1;1H')
-      const [freeRam, percentageFree] = calculateRAMUsage(totalRam)
-      colors.bold.log(`Free RAM: ${percentageFree}, ${freeRam}/${totalRam} GB`)
-    }, interval);
+  const uptime = () => {
+    const upTime = os.uptime()
+    const hours = Math.floor(upTime/3600)
+    const minutes = Math.floor((upTime%3600)/60)
+    return [hours, minutes]
   }
+
+  /*
+
+    !-- MONITORS --!
+
+  */
 
   const memoryMonitor = (totalRam) => {
     const [freeRam, percentageFree] = calculateRAMUsage(totalRam)
@@ -84,24 +82,35 @@ function systemMonitor(interval = 1000) {
     })
   }
 
-  const monitor = (interval) => {
-    const totalRam = Math.round(100*(os.totalmem()/1073741824))/100
-    let oldCpuTimes = os.cpus().map((cpu) => cpu.times)
+  const osMonitor = (osName) => {
+    const [hours, minutes] = uptime()
 
-    const monitorInterval = setInterval(() => {
+    console.log(`${osName}, running for ${hours} hours and ${minutes} minutes`)
+  }
+
+  const monitor = (interval) => {
+    //Constants to avoid redundancy
+    const totalRam = Math.round(100*(os.totalmem()/1073741824))/100
+    const osName = osType()
+
+    let oldCpuTimes = os.cpus().map((cpu) => cpu.times)
+    setInterval(() => {
       let cpuTimes = os.cpus().map((cpu) => cpu.times)
 
       process.stdout.write('\x1b[2J\x1b[1;1H')
+      colors.bold.bgmagenta.log("!--   CPU   --!")
       cpuMonitor(cpuTimes, oldCpuTimes)
+      colors.bold.bgmagenta.log("!-- MEMORY  --!")
       memoryMonitor(totalRam)
+      colors.bold.bgmagenta.log("!-- SYSTEM  --!")
+      osMonitor(osName)
+      
     }, interval);
   }
 
-  return {
-    cpus: () => cpus(interval),
-    memory: () => memory(interval),
-    all: () => monitor(interval)
-    }
+  return (interval = 1000) => {
+    monitor(interval)
+  }
 }
 
 module.exports = systemMonitor()
